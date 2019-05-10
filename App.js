d9c3ce3844dac2d1b96c7db444103aa385f32986
lhaggerty <lhaggerty@vt.ewsd.org>
2019-05-10 08:46:16 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Test 1</Text>
+        <Text>Test 2</Text>
+        <Text>Test 3</Text>
+        <Text>Test 4</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
